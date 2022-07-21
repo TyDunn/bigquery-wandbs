7f21659e0dd00e7ff695455f85670a5762202333
tydunn <t.dunn@rasa.com>
2022-07-21 17:30:02 +0200

    adding comments to script

--- bigquery-wandbs.py
+++ bigquery-wandbs.py
@@ -4,21 +4,29 @@ import wandb
 
 # BigQuery
 
+# set credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="secrets.json"
 
+# connect to BigQuery
 bqclient = bigquery.Client()
 
+# SQL query to grab 1000 streaming history entries
 query = "SELECT * FROM zinc-mantra-353207.demo_18b0_spotify.streaming_history LIMIT 1000"
 
+# query and convert to dataframe
 dataframe = (
     bqclient.query(query)
     .result()
     .to_dataframe(create_bqstorage_client=True)
 )
+
+# to show that it worked in command line
 print(dataframe.head())
 
 # Weights & Biases
 
+# intialize wandb project
 wandb.init(project="bigquery-wandbs")
 
+# add the data to wandb for visualization
 wandb.log({"table": dataframe})
